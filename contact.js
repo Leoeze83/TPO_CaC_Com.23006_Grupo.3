7d7d7d6d53582738d2725a08e6153d99c667caaa
stefi1236844555 <stefigava@gmail.com>
2023-05-23 15:00:57 -0300

    bastante bien

--- contact.js
+++ contact.js
@@ -1,39 +1,55 @@
-const nombre = document.getElementById("nombre")
-const correo = document.getElementById("correo")
-const telefono = document.getElementById("telefono")
-const mensaje = document.getElementById("mensaje")
-const form = document.getElementById("form")
 
-const alerta = document.getElementById("alerta")
+const formulario = document.getElementById('form');
+const nombreInput = document.getElementById('nombre');
+const correoInput = document.getElementById('correo');
+const telefonoInput = document.getElementById('telefono');
+const mensajeInput = document.getElementById('mensaje');
+const alerta = document.getElementById('alerta');
 
-form.addEventListener("submit", e=>{
-    e.preventDefault()
-    let alerta = ""
-    let entrar = false
-    let regexcorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
-    let regextelefono = Number
+formulario.addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita el envío automático del formulario
 
-    parrafo.innerHTML = ""
-    if(nombre.value.length <2){
-        alerta += `El nombre no es valido <br>`
-        entrar = true
-    }
-    if(!regexcorreo.test(correo.value)){
-        alerta += `El correo no es valido <br>`
-        entrar = true
-    }
+  if (nombreInput.value.trim() === '') {
+    mostrarAlerta('Por favor, ingresa tu nombre');
+    nombreInput.focus();
+    return;
+  }
 
-    if(!regextelefono.test(telefono.value)){
-        alerta += `El numero no es valido <br>`
-        entrar = true
-    }
-    if(mensaje.value.length <2){
-        alerta += `El nombre no es valido <br>`
-        entrar = true
-   
-    if(entrar){
-        alerta.innerHTML = alerta
-    }else{
-        alerta.innerHTML = "Enviado"
-    }
-})
+  if (correoInput.value.trim() === '') {
+    mostrarAlerta('Por favor, ingresa tu correo electrónico');
+    correoInput.focus();
+    return;
+  }
+
+  if (!validarCorreoElectronico(correoInput.value.trim())) {
+    mostrarAlerta('Por favor, ingresa un correo electrónico válido');
+    correoInput.focus();
+    return;
+  }
+
+  if (telefonoInput.value.trim() === '') {
+    mostrarAlerta('Por favor, ingresa tu número de teléfono');
+    telefonoInput.focus();
+    return;
+  }
+
+  if (mensajeInput.value.trim() === '') {
+    mostrarAlerta('Por favor, ingresa tu mensaje');
+    mensajeInput.focus();
+    return;
+  }
+
+  // Si llega hasta aquí, todos los campos son válidos
+  alerta.innerHTML = ''; // Limpia el mensaje de alerta si lo hay
+  formulario.submit(); // Envía el formulario
+});
+
+function mostrarAlerta(mensaje) {
+  alerta.innerHTML = mensaje;
+}
+
+function validarCorreoElectronico(correo) {
+  // Utiliza una expresión regular para validar el formato del correo electrónico
+  const expresionRegular = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return expresionRegular.test(correo);
+}
